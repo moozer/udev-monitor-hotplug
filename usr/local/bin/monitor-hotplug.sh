5132079f1c2d996dd4d7414d45892566dd47c8ad
moz <moz@local>
2016-09-29 22:39:17 +0200

    added check for mulitple displays...

--- usr/local/bin/monitor-hotplug.sh
+++ usr/local/bin/monitor-hotplug.sh
@@ -8,8 +8,15 @@ DEVICES=$(find /sys/class/drm/*/status)
 #inspired by /etc/acpd/lid.sh and the function it sources
 
 displaynum=`ls /tmp/.X11-unix/* | sed s#/tmp/.X11-unix/X##`
-display=":$displaynum"
-export DISPLAY=":$displaynum"
+if echo $displaynum | grep " " > /dev/null; then
+	logger "Error: multiple displays found: $displaynum"
+	exit 1
+fi
+
+display=":${displaynum}.0"
+export DISPLAY="$display"
+
+logger "Using DISPLAY $DISPLAY"
 
 #uid=$(ck-list-sessions | awk 'BEGIN { unix_user = ""; } /^Session/ { unix_user = ""; } /unix-user =/ { gsub(/'\''/,"",$3); unix_user = $3; } /x11-display = '\'$display\''/ { print unix_user; exit (0); }')
 #if [ -n "$uid" ]; then
